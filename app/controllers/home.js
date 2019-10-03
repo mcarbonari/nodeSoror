989170365c136fe126f9f780f02a02f4cedb3746
mcarbonari <mariaeduarda.s.carbonari@gmail.com>
2019-10-02 21:22:26 -0300

    bugs

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,7 @@ module.exports.index = function(app, req, res){
   res.render("home/index");
 }
 
-// module.exports.home = function (app, req, res) {
+// module.exports.index = function (app, req, res) {
 // 	if (!req.session.loggedin) {
 // 		res.render('index');
 // 	} else {
@@ -19,15 +19,15 @@ module.exports.index = function(app, req, res){
 // 			if (result.length > 0) {
 // 				req.session.loggedin = true;
 // 				req.session.email = email;
-				// res.redirect('/#perfil'); ???????????
-				// abreperfil(); ?????????????
+// 				res.redirect('/#perfil'); ???????????
+// 				abreperfil(); ?????????????
 // 			} else {
 // 				res.send('usuario ou senha incorretos')
 // 			}
 // 			res.end();
 // 		});
 // 	} else {
-	// res.send('por favor entre com o usuario e a senha');
-	// res.end();
+// 	res.send('por favor entre com o usuario e a senha');
+// 	res.end();
 // }
 // }
--- app/public/js/modal.js
+++ app/public/js/modal.js
@@ -1,12 +1,13 @@
 // Get the modal
 var modal = document.getElementById("myModal");
-
+ 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var aparecer = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
+var aparecer = document.getElementById("myButton");
+var modalTexto = document.getElementById("texto");
  aparecer.onclick = function(){
+ 	alert(" sahsuahsua")
   modal.style.display = "block";
-  modalImg.innerHTML = " aqui fica o texto completo sobre o assunto resumido da caixinha do carrossel; tem que falar bonito simples claro etc; tem que ver como vai ser pra abrir um texto em cada modal pq com esse código só funciona em um; sera que da pra colocar imagens? hmmm Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "; 
+  modalTexto.innerHTML = " aqui fica o texto completo sobre o assunto resumido da caixinha do carrossel; tem que falar bonito simples claro etc; tem que ver como vai ser pra abrir um texto em cada modal pq com esse código só funciona em um; sera que da pra colocar imagens? hmmm Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "; 
 }
 
 // Get the <span> element that closes the modal
