cb20f66e453f2176726752489deb6b0f666cdf02
mcarbonari <mariaeduarda.s.carbonari@gmail.com>
2019-10-15 01:38:36 -0300

    att

--- app/public/js/modal.js
+++ app/public/js/modal.js
@@ -5,9 +5,9 @@ var modal = document.getElementById("myModal");
 var aparecer = document.getElementById("myButton");
 var modalTexto = document.getElementById("texto");
  aparecer.onclick = function(){
- 	alert(" sahsuahsua")
+ 	// alert(" sahsuahsua")
   modal.style.display = "block";
-  modalTexto.innerHTML = " aqui fica o texto completo sobre o assunto resumido da caixinha do carrossel; tem que falar bonito simples claro etc; tem que ver como vai ser pra abrir um texto em cada modal pq com esse código só funciona em um; sera que da pra colocar imagens? hmmm Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "; 
+  // modalTexto.innerHTML = " aqui fica o texto completo sobre o assunto resumido da caixinha do carrossel; tem que falar bonito simples claro etc; tem que ver como vai ser pra abrir um texto em cada modal pq com esse código só funciona em um; sera que da pra colocar imagens? hmmm Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "; 
 }
 
 // Get the <span> element that closes the modal
