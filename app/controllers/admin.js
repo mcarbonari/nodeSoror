85f3efdd4020eb9f4ca9f1e14d59d9adf0d2684a
mcarbonari <mariaeduarda.s.carbonari@gmail.com>
2019-11-14 15:36:49 -0300

    a

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -31,6 +31,31 @@ module.exports.soror_salvar = function(app, req, res){
 	});
 }
 
+module.exports.soror_salvarservico = function(app, req, res){
+  var usuario = req.body;
+  console.log(usuario);
+
+  req.assert('nome', 'Nome é obrigatório').notEmpty();
+	req.assert('email', 'Email é obrigatório').isEmail();
+	req.assert('senha', 'Senha é obrigatório').notEmpty();
+	req.assert('confirmar_senha', 'Confirmar senha é obrigatório').notEmpty();
+	req.assert('cpf', 'CPF inválido').len(14, 14);
+
+	var erros = req.validationErrors();
+	if (erros){
+		console.log(erros);
+		res.render("home/index", {validacao: erros});
+		return;
+	}
+
+  var connection = app.config.dbConnection();
+	var sororModel = new app.app.models.sororDAO(connection);
+
+	sororModel.salvarUsuario(usuario, function(error, result){
+		res.redirect('/#cad');
+	});
+}
+
 module.exports.logout_usuario=function(app, req, res){
 	req.session.destroy();
 		res.redirect('/');
